057e6f0c4b8d4c65c9b96d68084c62ffdfab471b
rehman922 <rehman_922@yahoo.com>
2025-12-30 10:34:48 +0300

    gitignore remove gitconfig from repo

--- obj/Debug/net9.0/YusurIntegration.AssemblyInfo.cs
+++ obj/Debug/net9.0/YusurIntegration.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YusurIntegration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a446cafa7eeb9d34531223cfb07e85ed94a41a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca3b7ec973e646c1603e28ed3b1fd4f8d61d2297")]
 [assembly: System.Reflection.AssemblyProductAttribute("YusurIntegration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YusurIntegration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
